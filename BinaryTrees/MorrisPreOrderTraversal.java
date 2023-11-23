961bc0af522f381d5a3548703eb50ca0cfbe6c19
MaheshG <maheshgorur121492123@gmail.com>
2023-11-23 14:08:49 +0530

    new changes

---
+++ BinaryTrees/MorrisPreOrderTraversal.java
@@ -0,0 +1,52 @@
+/*https://www.codingninjas.com/studio/problems/preorder-traversal_3838888?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=1*/
+import java.util.* ;
+import java.io.*; 
+/*
+	
+	Following is the Binary Tree node structure:
+
+	public class TreeNode {
+	    int data;
+	    TreeNode left;
+	    TreeNode right;
+	    TreeNode() {}
+	    TreeNode(int val) { this.data = val; }
+	    TreeNode(int val, TreeNode left, TreeNode right) {
+	       this.data = val;
+	        this.left = left;
+	        this.right = right;
+	    }
+	}
+
+*/
+
+public class Solution {
+	// Threaded Binary Tree Approach | TC O(2n) | SC O(1)
+    public static List < Integer > getPreOrderTraversal(TreeNode root) {
+    	// Write your code here.
+		List<Integer> list = new ArrayList<>();
+        while(root != null){
+            TreeNode leftNode = root.left;
+            if(leftNode == null){
+                list.add(root.data);
+                root = root.right;
+            }else{
+                while(leftNode.right != null){
+                    if(leftNode.right == root)
+                        break;
+                    leftNode = leftNode.right;
+                }
+                if(leftNode.right == root){
+                    leftNode.right = null;
+                    root = root.right;
+                }else{
+                    leftNode.right = root;  
+					list.add(root.data);         
+                    root = root.left;
+                }
+            }
+        }
+        return list;
+    }
+	
+}
